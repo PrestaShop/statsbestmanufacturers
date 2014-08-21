ad79b3782241925c632fc0bd0c5e764b83b216d5
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-08-21 14:33:02 +0200

    //Version update

--- statsbestmanufacturers.php
+++ statsbestmanufacturers.php
@@ -41,7 +41,7 @@ class StatsBestManufacturers extends ModuleGrid
 	{
 		$this->name = 'statsbestmanufacturers';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.3';
+		$this->version = '1.4.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
