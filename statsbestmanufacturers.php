5a1be0fc6c4c0796e454ca563887eadff4d4f675
antoin_m <michel.br.antoine@gmail.com>
2017-01-11 10:16:14 +0100

    Update max version compliancy

--- statsbestmanufacturers.php
+++ statsbestmanufacturers.php
@@ -78,7 +78,7 @@ class StatsBestManufacturers extends ModuleGrid
 
 		$this->displayName = $this->l('Best manufacturers');
 		$this->description = $this->l('Adds a list of the best manufacturers to the Stats dashboard.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.7.0.99');
 	}
 
 	public function install()
