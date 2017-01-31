b9897beda6084d08f489d4d00995a085b8e76739
Maxime Biloé <maxime.biloe@gmail.com>
2017-01-31 18:03:07 +0100

    // Version updated to v2.0.0

--- statsbestmanufacturers.php
+++ statsbestmanufacturers.php
@@ -42,7 +42,7 @@ class statsbestmanufacturers extends ModuleGrid
     {
         $this->name = 'statsbestmanufacturers';
         $this->tab = 'analytics_stats';
-        $this->version = '1.5.0';
+        $this->version = '2.0.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
